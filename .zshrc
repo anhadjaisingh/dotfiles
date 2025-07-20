9fdc24983be46d7480a12f6ec4bbde00cc0b3db0
Anhad Jai Singh <ffledgling+github@gmail.com>
2025-07-20 15:08:56 -0700

    Update dotfiles for nvim, zsh and tmux
    * Tweak tmux splits to support same dir
    * Better autocomplete for zsh
    * Setup a bunch of plugins and lsp stuff for neovim.

--- .zshrc
+++ .zshrc
@@ -1,8 +1,15 @@
 # Enable colored output for ls
 alias ls='ls -G'
+alias vim='nvim'
+
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
 
 # Custom prompt with colored brackets based on last command status
 setopt PROMPT_SUBST
 PROMPT='%(?,%F{green}[%f,%F{red}[%f)%n@%F{240}%m%f %1~ %(?,%F{green}]%f,%F{red}]%f)$ '
+
+# Enable smart autocompletion
+autoload -Uz compinit && compinit
+# Enable case insensitive matching
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
--- init.lua
+++ init.lua
@@ -1,6 +1,20 @@
 -- Neovim Configuration in Lua
 -- Basic settings extracted from vim config
 
+-- Bootstrap lazy.nvim plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 -- Formatting and Indentation
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
@@ -13,6 +27,14 @@ vim.opt.backspace = {'indent', 'eol', 'start'}
 
 -- Line Numbers
 vim.opt.number = true
+vim.opt.relativenumber = false
+
+-- Current line highlighting
+vim.opt.cursorline = true
+
+-- Colors and appearance
+vim.opt.background = "dark"
+vim.opt.termguicolors = false
 
 -- Search Options
 vim.opt.magic = true
@@ -32,3 +54,131 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.softtabstop = 2
   end,
 })
+
+-- Plugin setup
+require("lazy").setup({
+  -- LSP Configuration
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "pyright" },
+      })
+      
+      local lspconfig = require("lspconfig")
+      
+      -- Python LSP (Pyright)
+      lspconfig.pyright.setup({
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              diagnosticMode = "workspace",
+              useLibraryCodeForTypes = true,
+              typeCheckingMode = "basic"
+            }
+          }
+        }
+      })
+      
+      -- LSP keybindings
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(ev)
+          local opts = { buffer = ev.buf }
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+        end,
+      })
+    end,
+  },
+  
+  -- Autocompletion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+          { name = 'path' },
+        })
+      })
+    end,
+  },
+  
+  -- Syntax highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "python", "lua", "vim", "bash" },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+  
+  -- Python-specific virtual environment support
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("venv-selector").setup({
+        auto_refresh = true,
+      })
+    end,
+    keys = {
+      { "<leader>vs", "<cmd>VenvSelect<cr>", desc = "Select virtual environment" },
+    },
+  },
+  
+  -- Colorschemes
+  { "marko-cerovac/material.nvim" },
+  { "folke/tokyonight.nvim" },
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "EdenEast/nightfox.nvim" },
+  { "sainnhe/everforest" },
+})
+
+-- Set default colorscheme
+vim.cmd.colorscheme("material")
